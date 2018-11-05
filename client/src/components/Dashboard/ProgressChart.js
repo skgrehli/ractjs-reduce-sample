36ac6d3f355659d41174e96331acb5a13e56a202
abhishek-blockseed <abhishek@blockseedinvestments.com>
2018-11-05 23:45:46 +0530

    update reatings charts

--- client/src/components/Dashboard/ProgressChart.js
+++ client/src/components/Dashboard/ProgressChart.js
@@ -5,6 +5,7 @@ import 'react-circular-progressbar/dist/styles.css';
 /* TODO :
 1. Pass Style in props from parent
 2. Pass container width also from parent as props
+3. Fix color prop ---> Not working currently --> Need Custom CSS
 */
 
 class ProgressChart extends Component {
@@ -13,7 +14,7 @@ class ProgressChart extends Component {
 	}
 
 	render() {
-		const { percentage, container_size } = this.props;
+		const { percentage, container_size, color } = this.props;
 		return(
 			<div class="container" style={{ width: `${container_size}` }}>
 			<CircularProgressbar
@@ -21,7 +22,7 @@ class ProgressChart extends Component {
 			  text={`${percentage}%`}
 			  initialAnimation={true}
 			  styles={{
-			  path: { stroke: `rgba(62, 152, 199, ${percentage / 100})` },
+			  path: { stroke: `rgba(color[0], color[1], color[2], ${percentage / 100})` },
 			  }}
 			/>
 			</div>
--- client/src/components/Dashboard/Ratings.js
+++ client/src/components/Dashboard/Ratings.js
@@ -2,41 +2,44 @@ import React, { Component } from 'react';
 import ProgressChart from './ProgressChart.js';
 
 /*TODO :
-1. This is an example --> build ratings container component properly for dashboard as per design
+1. Improve design
 2. Pass style in props to child ProgressChart component
+3. Build a wrapper around this and include headers etc as in design
 */
 
 class Ratings extends Component {
 	render() {
 		return(
-			<div class="container">
-				<div class="container-fluid">
-					<div class="container-fluid">
-						<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+			<div class="container-fluid">
+				<div class="row">
+						<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
 							<ProgressChart 
 								percentage={44}
 								container_size={"230px"}
+								color={[282, 222, 152]}
 							/>
 						</div>
-					</div>
-					<div class="container-fluid">
+					<div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
 						<div class="row">
 							<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
 								<ProgressChart 
 									percentage={88}
 									container_size={"130px"}
+									color={[162, 52, 299]}
 								/>
 							</div>
 							<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
 								<ProgressChart 
 									percentage={77}
 									container_size={"130px"}
+									color={[362, 152, 299]}
 								/>
 							</div>
 							<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
 								<ProgressChart 
 									percentage={66}
 									container_size={"130px"}
+									color={[362, 152, 299]}
 								/>
 							</div>
 						</div>
@@ -45,18 +48,21 @@ class Ratings extends Component {
 								<ProgressChart 
 									percentage={88}
 									container_size={"130px"}
+									color={[362, 152, 299]}
 								/>
 							</div>
 							<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
 								<ProgressChart 
 									percentage={77}
 									container_size={"130px"}
+									color={[362, 152, 299]}
 								/>
 							</div>
 							<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
 								<ProgressChart 
 									percentage={66}
 									container_size={"130px"}
+									color={[362, 152, 299]}
 								/>
 							</div>
 						</div>
