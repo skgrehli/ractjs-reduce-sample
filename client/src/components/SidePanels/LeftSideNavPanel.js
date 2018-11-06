5689e5823a8d402d239fa7b7ca92a96962e5fe38
abhishek-blockseed <abhishek@blockseedinvestments.com>
2018-11-06 17:05:18 +0530

    naive implementation of side panel

--- client/src/components/SidePanels/LeftSideNavPanel.js
+++ client/src/components/SidePanels/LeftSideNavPanel.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
+import { slide as Menu } from 'react-burger-menu';
+import './SidePanel.css';
 
-class LeftSideNavPanel extends Component {
-	render() {
-		return (
-			<div class="container">
-			<div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
-				<div class="row">
-					<img src={require('../../assets/logo/logo.png')} class="img-circle img-responsive" alt="Company Logo" />
-				</div>
-				<div class="row">					
-				</div>
-				<div class="row">
-					<p>IT</p>
-				</div>
-				<div class="row">
-					<p>IT</p>
-				</div>
-				<div class="row">
-					<p>IT</p>
-				</div>
-			</div>
-			</div>
-			);
-	}
+/*TODO:
+1. Improve this component and re-use in dashboard/InvestorList/StartupList
+*/
+
+class LeftSideNavPanel extends React.Component {
+  showSettings = (event) => {
+    event.preventDefault();
+  }
+
+  render () {
+    return (
+      <Menu class="bm-menu"
+      	isOpen={false}
+      	customBurgerIcon={ <img src="https://img.icons8.com/metro/50/000000/menu.png" class="bm-burger-button"/>}
+      	customCrossIcon={ <img src="https://img.icons8.com/ios/50/000000/multiply.png" class="bm-cross-button"/>}
+      >
+        <a id="home" className="menu-item" href="/forgot-password">Home</a>
+        <a id="about" className="menu-item" href="/forgot-password">About</a>
+        <a id="contact" className="menu-item" href="/forgot-password">Contact</a>
+        <a onClick={ this.showSettings } className="menu-item--small" href="/forgot-password">Settings</a>
+      </Menu>
+    );
+  }
 }
 
 export default LeftSideNavPanel;
